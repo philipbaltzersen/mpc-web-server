a469f0127a66d208e4dbabf1445bb5d7b5e62667
Philip Holm <philipbaltzersen@gmail.com>
2024-03-18 12:11:20 +1100

    Add models module

---
+++ app/src/models.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class StatisticalMethod(Enum):
+    INDEPENDENT_T_TEST = "independent t-test"
+
+
+class Analysis(BaseModel):
+    id: str | None
+    owners: list[str]
+    file_names: list[str]
+    statistical_method: StatisticalMethod
+    method_arguments: dict[str, str | int | float | bool] | None = None
+
