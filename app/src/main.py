b8ee723881596998f3d7cbcec63b9c2704f84d0b
Philip Holm <philipbaltzersen@gmail.com>
2024-03-18 13:25:19 +1100

    Finish endpoints for creating and reading analyses

--- app/src/main.py
+++ app/src/main.py
@@ -1,37 +1,48 @@
-import pandas as pd
-import scipy.stats as sp
-from fastapi import FastAPI
+import uuid
 
-from .connect import connect
+import psycopg2
+from fastapi import Depends, FastAPI, HTTPException
+
+from .connect import get_conn
 from .models import Analysis
 
 app = FastAPI()
 
-conn = connect()
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
 @app.post("/analyses/")
-def add_analysis(analysis: Analysis):
+def add_analysis(analysis: Analysis, conn = Depends(get_conn)) -> Analysis:
+    if analysis.id is None:
+        analysis.id = str(uuid.uuid4())
+    
+    query = "INSERT INTO analyses (id, owners, file_names, statistical_method) VALUES (%s, %s, %s, %s);"
+    
+    with conn.cursor() as cur:
+        cur.execute(query, (analysis.id, analysis.owners, analysis.file_names, analysis.statistical_method.value))
+        conn.commit()
+        
     return analysis
 
 
-@app.get("/data/{owner}")
-def read_data(owner: str):
+@app.get("/analyses/{analysis_id}")
+def get_analysis(analysis_id: str, conn = Depends(get_conn)) -> Analysis:
     with conn.cursor() as cur:
-        cur.execute(f"SELECT * FROM data WHERE owner = '{owner}';")
-        result = cur.fetchall()
-    conn.close()
-    return result
+        try:
+            cur.execute(f"SELECT * FROM analyses WHERE id = '{analysis_id}';")
+        except psycopg2.errors.InvalidTextRepresentation:
+            raise HTTPException(status_code=400, detail="Invalid UUID format")
+        result = cur.fetchone()
 
-@app.get("/analyze")
-def analyze_data():
-    with conn.cursor() as cur:
-        cur.execute("SELECT data->'before', data->'after' FROM data;")
-        df = pd.DataFrame(cur.fetchall(), columns=["before", "after"])
+    if not result:
+        raise HTTPException(status_code=404, detail="Analysis not found")
     
-    statistic, p_value = sp.ttest_ind(df["before"], df["after"])
-    conn.close()
-    return {"statistic": statistic, "p_value": p_value}
+    return Analysis(
+        id=result[0],
+        owners=result[1],
+        file_names=result[2],
+        statistical_method=result[3],
+        method_arguments=result[4],
+    )
