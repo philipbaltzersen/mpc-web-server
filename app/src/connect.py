a34c7dc41fce3a868fc66257a9cfc8aa203f2c9c
Philip Holm <philipbaltzersen@gmail.com>
2024-03-18 13:18:52 +1100

    Yield connection

--- app/src/connect.py
+++ app/src/connect.py
@@ -1,16 +1,18 @@
 import psycopg2  # type: ignore
 
 
-def connect():
+def get_conn():
     try:
-        with psycopg2.connect(
+        conn = psycopg2.connect(
             dbname="postgres",
             user="postgres",
             password="password",
             host="db",
             port="5432"
-        ) as conn:
-            print("Connected to database")
-            return conn
+        )
+        print("Connected to database")
+        yield conn
     except psycopg2.DatabaseError as e:
         print(f"Error {e}")
+    finally:
+        conn.close()
