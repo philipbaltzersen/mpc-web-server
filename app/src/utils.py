b686c582b84d081289cb13d7d1052551f89314c7
Philip Holm <philipbaltzersen@gmail.com>
2024-03-20 10:36:17 +1100

    Parameterize load_dotenv

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,7 +1,7 @@
 import os
 
-def load_dotenv():
-    with open("/.env") as file:
+def load_dotenv(path: str = "/.env"):
+    with open(path) as file:
         for line in file:
             if line.strip() and not line.startswith("#"):
                 key, value = line.strip().split("=")
