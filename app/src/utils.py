a66c2ab70703c71b855d2c09a95d79b882c98534
Philip Holm <philipbaltzersen@gmail.com>
2024-04-25 19:50:05 +0200

    Add encrypt/decrypt methods

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,8 +1,39 @@
 import os
 
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
 def load_dotenv(path: str = "/.env"):
     with open(path) as file:
         for line in file:
             if line.strip() and not line.startswith("#"):
                 key, value = line.strip().split("=")
                 os.environ[key] = value
+
+
+def encrypt(data: bytes, key: bytes) -> bytes:
+    """Used by client to encrypt data locally with enclaves public key"""
+    data_key = AESGCM.generate_key(bit_length=256)
+    aes_data = AESGCM(data_key)
+    nonce_data = os.urandom(12)
+    ciphertext = aes_data.encrypt(nonce_data, data, None)
+
+    aes_key = AESGCM(key)
+    nonce_key = os.urandom(12)
+    enc_data_key = aes_key.encrypt(nonce_key, data_key, None)
+
+    return nonce_key + enc_data_key + nonce_data + ciphertext
+
+
+def decrypt(message: bytes, key: bytes) -> bytes:
+    """Used by enclave to decrypt data"""
+    nonce_key = message[:12]
+    enc_data_key = message[12:60]
+    nonce_data = message[60:72]
+    ciphertext = message[72:]
+
+    aes_key = AESGCM(key)
+    data_key = aes_key.decrypt(nonce_key, enc_data_key, None)
+
+    aes_data = AESGCM(data_key)
+    return aes_data.decrypt(nonce_data, ciphertext, None)
